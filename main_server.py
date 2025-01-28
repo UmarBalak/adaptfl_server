8dd8d4dfc25ce281c8ef3b6a5aa4d68646dc6953
Umar Balak <umarbalak35@gmail.com>
2025-01-28 20:40:47 +0530

    silly mistake lolz

--- main_server.py
+++ main_server.py
@@ -780,7 +780,7 @@ def scheduled_aggregate_weights():
 
 scheduler.start()
 
-if __name__ == "__main__":
-    import uvicorn
-    logging.info("Starting Server...")
-    uvicorn.run(app, host="localhost", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     logging.info("Starting Server...")
+#     uvicorn.run(app, host="localhost", port=8000)
